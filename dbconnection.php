8625851c92db5783df4278f4a5730bdda58eb78e
jishnucm <jishnucm55@gmail.com>
2022-07-18 17:27:17 +0530

    first change

--- dbconnection.php
+++ dbconnection.php
@@ -7,6 +7,3 @@ $connection=mysqli_connect($servername,$username,$password,$dbname);
 if(! $connection){
     die ("unable to connect".mysqli_connect_error());
 }
-// else{
-//     echo "connection sucess";
-// }
